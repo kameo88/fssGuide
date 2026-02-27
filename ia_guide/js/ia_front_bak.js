66a64ab46b001896716c65fa91b1e3e60c556422
kameo <kameo88@gmail.com>
2026-02-27 10:27:38 +0900

    "주석처리" 관련 일관성 적용

--- ia_guide/js/ia_front_bak.js
+++ ia_guide/js/ia_front_bak.js
@@ -1,16 +1,39 @@
-window.addEventListener('load', function() {
-			var allElements = document.getElementsByTagName('*');
-			Array.prototype.forEach.call(allElements, function(el) {
-					var includePath = el.dataset.includePath;
-					if (includePath) {
-							var xhttp = new XMLHttpRequest();
-							xhttp.onreadystatechange = function () {
-									if (this.readyState == 4 && this.status == 200) {
-										el.outerHTML = this.responseText;
-									}
-							};
-							xhttp.open('GET', includePath, true);
-							xhttp.send();
-					}
-			});
+// window.addEventListener('load', function() {
+// 			var allElements = document.getElementsByTagName('*');
+// 			Array.prototype.forEach.call(allElements, function(el) {
+// 					var includePath = el.dataset.includePath;
+// 					if (includePath) {
+// 							var xhttp = new XMLHttpRequest();
+// 							xhttp.onreadystatechange = function () {
+// 									if (this.readyState == 4 && this.status == 200) {
+// 										el.outerHTML = this.responseText;
+// 									}
+// 							};
+// 							xhttp.open('GET', includePath, true);
+// 							xhttp.send();
+// 					}
+// 			});
+// });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const includes = document.querySelectorAll("[data-include]");
+
+    includes.forEach(el => {
+        const file = el.getAttribute("data-include");
+
+        fetch(file)
+            .then(res => res.text())
+            .then(html => {
+                el.innerHTML = html;
+
+                // include 완료 후 이벤트 트리거
+                document.dispatchEvent(new Event("include:loaded"));
+            })
+            .catch(err => {
+                console.error("Include Error:", err);
+            });
+    });
+
 });
